5b421eebf3f5d9c9f6da6fd4e941c10853f0fd3c
unknown <arturstylus@gmail.com>
2016-01-24 15:29:08 +0200

    Add Communication folder

---
+++ ChatCommunication/ICommunication.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace ChatCommunication
+{
+    [ServiceContract]
+    public interface ICommunication
+    {
+        [OperationContract(IsOneWay = true)]
+        void DisplayMessage(SendEntity someEntity);
+
+        void SendMessage(string text);
+    }
+}
