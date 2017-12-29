ddd0833fcea6108dce1c813c8a10001b4a50089e
Artur <arturstylus@gmail.com>
2017-12-29 22:18:05 +0200

    Update Communication.cs

--- ChatCommunication/Communication.cs
+++ ChatCommunication/Communication.cs
@@ -46,7 +46,7 @@ namespace ChatCommunication
         private ChannelFactory<ICommunication> channelFactory;
         private ICommunication _channel;
 
-        
+        //TODO:rewrite SendMessage Method
         public void SendMessage(string text)
         {
             if (text.StartsWith("setname:", StringComparison.OrdinalIgnoreCase))
